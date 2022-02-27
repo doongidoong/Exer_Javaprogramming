6a5fc25c0bbff23edf0fe7838741d0f55ad5ec7c
Jihun Lee <wlgns3398@naver.com>
2022-02-28 01:23:25 +0900

    메소드 활용 구구단

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 
 public class Gugudan {
+    public  static int[] calculate(int times) {
+        int[] result = new int[9];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = times* (i + 1);
+        }
+        return result;
+    }
+    public static void print(int[] result){
+        for(int i=0;i<result.length;i++){
+            System.out.println(result[i]);
+        }
+    }
     public static  void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         System.out.println("사용자가 입력한 값 : "+ number);
         if(number>=2 && number<=9){
-        for(int i=1;i<10;i++){
-            System.out.print(number*i+" ");
-        }}
+            int[] time = calculate(number);
+            print(time);
+            }
         else{
             System.out.println("값을 잘못 입력했습니다.");
         }
-
     }
 }
